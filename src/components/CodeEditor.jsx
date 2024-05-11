5cec3dcdd417ae9452a49f58863284db6d2f6e1e
Rajat Sinha <111138210+Rajatsinha05@users.noreply.github.com>
2024-05-11 21:52:14 +0530

    code working

--- src/components/CodeEditor.jsx
+++ src/components/CodeEditor.jsx
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from "react";
-import { Box, Button, VStack, HStack } from "@chakra-ui/react";
+import { Box, HStack, Button, Spacer } from "@chakra-ui/react";
 import { Editor } from "@monaco-editor/react";
 import LanguageSelector from "./LanguageSelector";
 import { CODE_SNIPPETS } from "../constants";
@@ -40,39 +40,52 @@ const CodeEditor = () => {
     });
   };
 
+  const handleRunClick = () => {
+   
+  };
+
+  const handleSubmitClick = () => {
+    
+  };
+
   return (
-    <VStack spacing={4} align="stretch" p={4}>
-      <LanguageSelector language={language} onSelect={onSelect} />
-      <HStack spacing={4} align="stretch">
-        <Box flex={1}>
-          <Editor
-            options={{
-              minimap: { enabled: false },
-              fontSize: 14,
-              wordWrap: "on",
-              suggest: {
-                showWords: true,
-                showMethods: true,
-                showFunctions: true,
-              },
-            }}
-            height="75vh"
-            theme="vs-dark"
-            language={language}
-            defaultValue={CODE_SNIPPETS[language]}
-            onMount={(editor, monaco) => {
-              onMount(editor);
-              onSuggestions(monaco, language);
-            }}
-            value={value}
-            onChange={(value) => setValue(value)}
-          />
+    <Box p={4}>
+      <Box >
+        text
+      </Box>
+      <HStack spacing={4}>
+        <Box w="50%">
+          <LanguageSelector language={language} onSelect={onSelect} />
+          <Box bg="gray.800" borderRadius="md" p={4}>
+            <Editor
+              options={{
+                minimap: { enabled: false },
+                fontSize: 14,
+                wordWrap: "on",
+                suggest: {
+                
+                  showWords: true,
+                  showMethods: true,
+                  showFunctions: true,
+                },
+              }}
+              height="75vh"
+              theme="vs-dark" 
+              language={language}
+              defaultValue={CODE_SNIPPETS[language]}
+              onMount={(editor, monaco) => {
+                onMount(editor);
+                onSuggestions(monaco, language);
+              }}
+              value={value}
+              onChange={(value) => setValue(value)}
+            />
+          </Box>
         </Box>
-        <Box flex={1}>
-          <Output editorRef={editorRef} language={language} />
-        </Box>
+        <Output editorRef={editorRef} language={language} />
       </HStack>
-    </VStack>
+
+    </Box>
   );
 };
 
