9483adeee5c3fa984868f42b6bf3f59105bffb3b
Rajat Sinha <111138210+Rajatsinha05@users.noreply.github.com>
2024-06-05 02:18:48 +0530

    question by id

--- src/Pages/SingleProblem.jsx
+++ src/Pages/SingleProblem.jsx
@@ -1,10 +1,10 @@
 import { Box } from '@chakra-ui/react';
 import React from 'react';
 import CodeEditor from '../components/CodeEditor';
-import { useParams } from 'react-router-dom'; // Import useParams hook
+import { useParams } from 'react-router-dom'; 
 
 const SingleProblem = () => {
-  const { id } = useParams(); // Access route parameters using useParams hook
+  const { id } = useParams(); 
 
   return (
     <Box minH="100vh" bg="#0f0a19" color="gray.500" px={6} py={8}>
