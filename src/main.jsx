77116505512b3b3d3dddcdf85062f2393d45cdea
Rajat Sinha <111138210+Rajatsinha05@users.noreply.github.com>
2024-10-06 09:44:20 +0530

    navbar error solved adn ranking added

--- src/main.jsx
+++ src/main.jsx
@@ -5,18 +5,18 @@ import { ChakraProvider } from "@chakra-ui/react";
 import theme from "./theme.js";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
-import { BrowserRouter  } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-   
-    <Provider store={store}>
 
-  
-    <ChakraProvider theme={theme}>
-      <App />
-    </ChakraProvider>
-    </Provider>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Provider store={store}>
+          <ChakraProvider theme={theme}>
+            <App />
+          </ChakraProvider>
+        </Provider>
+      </BrowserRouter>
+ 
   </React.StrictMode>
 );
